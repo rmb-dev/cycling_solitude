26c9660a260a3743c09d19ded455a52e456af00f
Roman <rmb_dev@protonmail.com>
2022-05-12 18:46:21 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 /*====== MENU =====*/
-const showMenu = (toggleId,navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*====== RELLAX =====*/
 var rellax = new Rellax('.parallax');
@@ -17,16 +17,16 @@ var rellax = new Rellax('.parallax');
 
 /*====== ANIMATE GSAP ======*/
 /*Navbar*/
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
-gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+gsap.from('.nav__logo', { opacity: 0, duration: 3, delay: .5, y: 30, ease: 'expo.out' });
+gsap.from('.nav__toggle', { opacity: 0, duration: 3, delay: .7, y: 30, ease: 'expo.out' });
+gsap.from('.nav__item', { opacity: 0, duration: 3, delay: .7, y: 35, ease: 'expo.out', stagger: .2 })
 
 /*Text*/
-gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
-gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+gsap.from('.home__title', { opacity: 0, duration: 3, delay: 1.3, y: 35, ease: 'expo.out' });
+gsap.from('.home__subtitle', { opacity: 0, duration: 3, delay: 1.1, y: 35, ease: 'expo.out' });
 
 /*Scroll*/
-gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
+gsap.from('.home__scroll', { opacity: 0, duration: 3, delay: 1.5, y: 25, ease: 'expo.out' });
 
 
 /*====== SCROLL REVEAL SECTION ======*/
@@ -36,7 +36,7 @@ const sr = ScrollReveal({
 });
 
 /*Data*/
-sr.reveal('.section__data',{origin: 'left',distance: '70px'}); 
+sr.reveal('.section__data', { origin: 'left', distance: '70px' });
 
 /*Imgs*/
-sr.reveal('.section__img',{origin: 'left',distance: '90px',delay: 200}); 
+sr.reveal('.section__img', { origin: 'left', distance: '90px', delay: 200 }); 
